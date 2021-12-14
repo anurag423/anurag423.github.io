2e93962a50e60bed73942488a2436cba02f8dfed
Anurag Numboori <anumboori@hotmail.com>
2021-12-14 02:51:23 -0500

    looks good but lacking functionality

---
+++ static/main.js
@@ -0,0 +1,9 @@
+const parallax = document.querySelectorAll(".parallax");
+//console.log(parallax)
+window.addEventListener("scroll", function() {
+    let offset = window.pageYOffset;
+    parallax.forEach(function(prllx, i) {
+        //console.log("Parallax " + i + ": " + prllx.offsetTop + " / Window: " + offset);
+        prllx.style.backgroundPositionY = (offset - prllx.offsetTop) * 0.7 + "px";
+    })
+})
