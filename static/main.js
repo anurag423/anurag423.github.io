eb49e30c981f526bf98ef3545bd1235d77adcf7f
Anurag Numboori <anumboori@hotmail.com>
2021-12-14 16:14:53 -0500

    cleaned up code. fixed mobile probably

--- static/main.js
+++ static/main.js
@@ -1,9 +1,11 @@
-const parallax = document.querySelectorAll(".parallax");
-//console.log(parallax)
-window.addEventListener("scroll", function() {
-    let offset = window.pageYOffset;
-    parallax.forEach(function(prllx, i) {
-        //console.log("Parallax " + i + ": " + prllx.offsetTop + " / Window: " + offset);
-        prllx.style.backgroundPositionY = (offset - prllx.offsetTop) * 0.7 + "px";
+if (window.innerWidth > 1110) {
+    const parallax = document.querySelectorAll(".parallax");
+    console.log(parallax)
+    window.addEventListener("scroll", function() {
+        let offset = window.pageYOffset;
+        parallax.forEach(function(prllx, i) {
+            //console.log("Parallax " + i + ": " + prllx.offsetTop + " / Window: " + offset);
+            prllx.style.backgroundPositionY = (offset - prllx.offsetTop) * 0.7 + "px";
+        })
     })
-})
+}
